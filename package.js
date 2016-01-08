ed9151648584fb55f49dc3ccb4e0446136ffdbd4
Ahmed Hassan <a.hassan@buismed.de>
2016-01-08 16:03:43 +0100

    inital commit

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+    name: 'nerdmed:sweet-alert',
+    version: '0.0.1',
+
+    // Brief, one-line summary of the package.
+    summary: 'Just raw sweet alert 2 lib',
+
+    // URL to the Git repository containing the source code for this package.
+    git: 'git@github.com:nerdmed/sweetalert.git',
+
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.addFiles('sweet-alert.js', 'client');
+});
